1202362c4039551fcd82e727ff4318aa96f82bea
Gautammm16 <gautamnjaisur@gmail.com>
2025-06-10 15:55:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,4 @@
-// import { useState, useEffect } from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import FlixtarLikeSPA from './pages/FlixtarSPA';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Loader from './components/Loader';
 
-// function App() {
-//   const [loading, setLoading] = useState(true);
-//   const [fadeOut, setFadeOut] = useState(false);
-
-//   useEffect(() => {
-//     document.body.classList.add('overflow-hidden', 'loader-active');
-
-//     const timer = setTimeout(() => {
-//       setFadeOut(true);
-//       setTimeout(() => {
-//         setLoading(false);
-//         document.body.classList.remove('overflow-hidden', 'loader-active');
-//       }, 500);
-//     }, 2000);
-
-//     return () => {
-//       clearTimeout(timer);
-//       document.body.classList.remove('overflow-hidden', 'loader-active');
-//     };
-//   }, []);
-
-//   if (loading) {
-//     return <Loader fadeOut={fadeOut} />;
-//   }
-
-//   return (
-//     <div className="min-h-screen flex flex-col App">
-//       <Header />
-//       <main className="flex-grow">
-//         <Routes>
-//           <Route path="/" element={<FlixtarLikeSPA />} />
-//         </Routes>
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 import { useState, useEffect } from 'react';
